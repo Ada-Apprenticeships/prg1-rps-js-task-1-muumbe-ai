b7b208b23bb29641f55855b91ed76bc83fdabe7f
muumbe-ai <muumbe.hapenga@ada.ac.uk>
2024-09-25 11:52:26 +0000

    added the || to seperate each option

--- rps.js
+++ rps.js
@@ -1,13 +1,11 @@
-//let player1 = "rock"
-//let player2 = "scissors"
 
 function rockPaperScissors(player1, player2) {
  if (player1 === player2){
-  return "draw"
+  return "draw" 
  }
  else if (
-  (player1 === "rock" && player2 === "scissors")
-  (player1 === "paper" && player2 === "rock")
+  (player1 === "rock" && player2 === "scissors") ||
+  (player1 === "paper" && player2 === "rock") ||
   (player1 === "scissors" && player2 === "paper") 
  ) {
   return 'player1'
