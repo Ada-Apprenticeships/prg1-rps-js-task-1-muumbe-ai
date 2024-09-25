e8fce2e432ee7cdc932eb317efb61c37330d673f
muumbe-ai <muumbe.hapenga@ada.ac.uk>
2024-09-25 11:57:21 +0000

    used prettier to tidy code

--- rps.js
+++ rps.js
@@ -1,30 +1,20 @@
-
 function rockPaperScissors(player1, player2) {
- if (player1 === player2){
-  return "draw" 
- }
- else if (
-  (player1 === "rock" && player2 === "scissors") ||
-  (player1 === "paper" && player2 === "rock") ||
-  (player1 === "scissors" && player2 === "paper") 
- ) {
-  return 'player1';
- } else {
-  return 'player2';
- }
-   
+  if (player1 === player2) {
+    return "draw";
+  } else if (
+    (player1 === "rock" && player2 === "scissors") ||
+    (player1 === "paper" && player2 === "rock") ||
+    (player1 === "scissors" && player2 === "paper")
+  ) {
+    return "player1";
+  } else {
+    return "player2";
+  }
 }
 
-console.log(rockPaperScissors('scissors','scissors'))
-
-
-
-
-
-
-
+console.log(rockPaperScissors("scissors", "scissors"));
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
