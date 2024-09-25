2c6f411bb4ff74cab03a083680aed4397ba080d6
muumbe-ai <muumbe.hapenga@ada.ac.uk>
2024-09-25 11:25:51 +0000

    2 tests passed so far.

--- rps.js
+++ rps.js
@@ -1,6 +1,11 @@
 function rockPaperScissors(player1, player2) {
+ if (player1 == player2){
+  return "draw"
+ }
+  else if (player1 == "paper" && player2 == "rock" ) {
+    return "player2"
+  }
 
-  
    
 }
 
